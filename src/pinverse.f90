56db61ee015dd805f2695547c60f194a7d4a743a
Seyed Ali Ghasemi <info@gha3mi.com>
2023-06-21 21:56:46 +0200

    Used forsvd module.

--- src/pinverse.f90
+++ src/pinverse.f90
@@ -1,22 +1,15 @@
 module pinverse
 
-   ! This module provides functions and subroutines
-   ! for calculating the singular value decomposition (SVD) and pseudoinverse of a matrix.
+   !This module provides functions and subroutines for pseudoinverse calculations.
 
-   use :: kinds  ! Import the module 'kinds' for precision types
+   use :: kinds
+   use :: forsvd, only: svd
 
    implicit none
    
-   private              ! Make all module variables and procedures private
+   private
 
-   public :: svd, pinv  ! Make svd and pinv functions public
-
-   !===============================================================================
-   interface svd
-      procedure :: svd_rel ! Interface for the svd_rel subroutine
-   end interface
-   !===============================================================================
-
+   public ::  pinv
 
    !===============================================================================
    interface pinv
@@ -25,53 +18,6 @@ module pinverse
    !===============================================================================
 
 contains
-
-   !===============================================================================
-   !> author: Seyed Ali Ghasemi
-   !> Calculates the singular value decomposition (SVD) of a matrix A.
-   pure subroutine svd_rel(A, U,S,VT)
-
-      ! Inputs:
-      real(rk), dimension(:, :), contiguous,          intent(in)  :: A    ! Input matrix A  
-
-      ! Outputs:
-      real(rk), dimension(size(A,1), size(A,1)),      intent(out) :: U    ! Left singular vectors
-      real(rk), dimension(size(A,2), size(A,2)),      intent(out) :: VT   ! Right singular vectors
-      real(rk), dimension(min(size(A,1), size(A,2))), intent(out) :: S    ! Singular values
-
-      ! Local variables
-      real(rk)                                                    :: work1(1) ! memory allocation query
-      real(rk), dimension(:), allocatable                         :: work     ! Work array
-      integer                                                     :: m, n, lwork, info
-
-      ! External subroutine for calculating the SVD
-      interface dgesvd
-         pure subroutine dgesvd(jobuf,jobvtf,mf,nf,af,ldaf,sf,uf,lduf,vtf,ldvtf,workf,lworkf,infof)
-            use kinds
-            character, intent(in)  :: jobuf, jobvtf
-            integer,   intent(in)  :: mf, nf, ldaf, lduf, ldvtf, lworkf
-            real(rk),  intent(in)  :: Af(ldaf, *)
-            real(rk),  intent(out) :: Sf(min(mf, nf))
-            real(rk),  intent(out) :: Uf(lduf, *), VTf(ldvtf, *)
-            real(rk),  intent(out) :: workf(*)
-            integer,   intent(out) :: infof
-         end subroutine dgesvd
-      end interface
-
-      m = size(A, 1)
-      n = size(A, 2)
-      
-      ! Calculate the optimal size of the work array
-      call dgesvd('S', 'S', m, n, A, m, S, U, m, VT, n, work1, -1, info)
-      lwork = nint(work1(1))
-      allocate(work(lwork))
-
-      call dgesvd('S', 'S', m, n, A, m, S, U, m, VT, n, work, lwork, info)
-
-      deallocate(work)
-   end subroutine svd_rel
-   !===============================================================================
-
 
    !===============================================================================
    !> author: Seyed Ali Ghasemi
@@ -84,7 +30,7 @@ contains
 
       ! Outputs:
       real(rk), dimension(size(A,2), size(A,1))          :: Apinv ! Pseudoinverse of A
-      
+
       ! Local variables
       real(rk), dimension(size(A,1), size(A,1))          :: U    ! Left singular vectors
       real(rk), dimension(size(A,2), size(A,2))          :: VT   ! Right singular vectors
